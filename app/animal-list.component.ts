04d60a6d9f99aa66fb85c97f090c38e5e9257e12
Dawson Mortenson <dawson.mortenson@gmail.com>
2017-10-27 13:33:23 -0700

    add an imgurl property to animals and render it as an image in the animal-list.component

--- app/animal-list.component.ts
+++ app/animal-list.component.ts
@@ -12,15 +12,24 @@ import { Animal } from './animal.model';
       </div>
       <div class="col-8">
         <div class="tab-content" id="nav-tabContent">
-          <div *ngFor="let angularAnimal of angularAnimals" class="tab-pane fade show " id="{{angularAnimal.name}}" role="tabpanel">
-            Name: {{angularAnimal.name}}
-            Age: {{angularAnimal.age}}
-            Diet: {{angularAnimal.diet}}
-            Location: {{angularAnimal.location}}
-            Caretakers: {{angularAnimal.caretakers}}
-            Sex: {{angularAnimal.sex}}
-            Likes: {{angularAnimal.likes}}
-            Dislikes: {{angularAnimal.dislikes}}
+          <div *ngFor="let angularAnimal of angularAnimals" class="tab-pane fade show" id="{{angularAnimal.name}}" role="tabpanel">
+            <div class="card card-body">
+              <h2 class="text-center">{{angularAnimal.name}} ({{angularAnimal.species}})</h2>
+              <div class="row">
+                <div class="col-lg-6 col-sm-12">
+                  <img src="{{angularAnimal.imgurl}}" class="img-fluid" alt="Responsive image">
+                </div>
+                <div class="col-lg-6 col-sm-12">
+                  <h4>Age: {{angularAnimal.age}}</h4>
+                  <h4>Diet: {{angularAnimal.diet}}</h4>
+                  <h4>Location: {{angularAnimal.location}}</h4>
+                  <h4>Caretakers: {{angularAnimal.caretakers}}</h4>
+                  <h4>Sex: {{angularAnimal.sex}}</h4>
+                  <h4>Likes: {{angularAnimal.likes}}</h4>
+                  <h4>Dislikes: {{angularAnimal.dislikes}}</h4>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -30,8 +39,8 @@ import { Animal } from './animal.model';
 
 export class AnimalListComponent {
   angularAnimals: Animal[] = [
-    new Animal("Gallus Gallus Domesticus","Roadrunner", 9, "Omnivore", "The Coop", 2, "Female", "Dust Baths", "Cats & Raccoons"),
-    new Animal("Ailuropoda melanoleuca", "Po", 7, "Herbivore", "Jade Palace", 3, "Male", "Dumplings", "Long walks"),
-    new Animal("Equus quagga", "Marty", 22, "Herbivore", "Plains Habitat", 2, "Male", "The Wild", "Captivity")
+    new Animal("Gallus Gallus Domesticus","Roadrunner", 9, "Omnivore", "The Coop", 2, "Female", "Dust Baths", "Cats & Raccoons", "https://i.pinimg.com/originals/7a/4c/08/7a4c0838d716459599ad4cf2b63727c4.jpg"),
+    new Animal("Ailuropoda Melanoleuca", "Po", 7, "Herbivore", "Jade Palace", 3, "Male", "Dumplings", "Long walks", "https://media4.s-nbcnews.com/j/newscms/2016_36/1685951/ss-160826-twip-05_8cf6d4cb83758449fd400c7c3d71aa1f.nbcnews-ux-2880-1000.jpg"),
+    new Animal("Equus Quagga", "Marty", 22, "Herbivore", "Plains Habitat", 2, "Male", "The Wild", "Captivity", "https://www.sa-venues.com/wildlife/graphics/zebra/1.jpg")
   ]
 }
