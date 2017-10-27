8a448835ac3d124cafa9690aa2b603d29c0c1719
Dawson Mortenson <dawson.mortenson@gmail.com>
2017-10-27 11:26:33 -0700

    add an animal model to the project

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,10 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-
+    <div class="container">
+      <div class="jumbotron">
+        <h1>{{title}}</h1>
+      </div>
+    </div>
   `
 })
 
 export class AppComponent {
-
+  title: string = "Angular Animals at the Zoo";
 }
